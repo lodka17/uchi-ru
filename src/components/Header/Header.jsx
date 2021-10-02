1cc7ff020882fea683bd9013f333b9ce7cd6e0d8
lodka17 <lodka17@gmail.com>
2021-10-02 03:09:05 +0300

    fixed key

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,59 +1,60 @@
-import React from 'react'
+import React from "react";
+import key from "weak-key";
 import styles from "./styles.module.scss";
 
 const navLeft = [
-    {
-        name: 'Личный кабинет',
-        link: '',
-    },
-    {
-        name: 'Форма постановки вопроса',
-        link: '',
-    },
-    {
-        name: 'Выбрать предмет',
-        link: '',
-    },
-    {
-        name: 'Архив',
-        link: '',
-    },
-]
+  {
+    name: "Личный кабинет",
+    link: "",
+  },
+  {
+    name: "Форма постановки вопроса",
+    link: "",
+  },
+  {
+    name: "Выбрать предмет",
+    link: "",
+  },
+  {
+    name: "Архив",
+    link: "",
+  },
+];
 
 const NavRight = [
-    {
-        name: 'Чат',
-        link: '',
-    },
-    {
-        name: 'Помощь',
-        link: '',
-    },
-]
+  {
+    name: "Чат",
+    link: "",
+  },
+  {
+    name: "Помощь",
+    link: "",
+  },
+];
 
 export const Header = () => (
-    <header className={styles.header}>
-        <div className={styles.container}>
-            <div className={styles.flex}>
-                <nav>
-                    <ul className={styles.list}>
-                        {
-                            navLeft.map((el) => (
-                                <li className={styles.left} key={el}><a href={el.link}>{el.name}</a></li>
-                            ))
-                        }
-                    </ul>
-                </nav>
-                <nav>
-                    <ul className={styles.list}>
-                        {
-                            NavRight.map((el) => (
-                                <li className={styles.right} key={el}><a href={el.link}>{el.name}</a></li>
-                            ))
-                        }
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </header>
-)
+  <header className={styles.header}>
+    <div className={styles.container}>
+      <div className={styles.flex}>
+        <nav>
+          <ul className={styles.list}>
+            {navLeft.map((el) => (
+              <li className={styles.left} key={key(el)}>
+                <a href={el.link}>{el.name}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <nav>
+          <ul className={styles.list}>
+            {NavRight.map((el) => (
+              <li className={styles.right} key={key(el)}>
+                <a href={el.link}>{el.name}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+);
--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -1,148 +1,142 @@
-import DateRangePicker from '@wojtekmaj/react-daterange-picker';
-import React, { useState } from 'react'
+// eslint-disable-next-line import/no-unresolved
+import DateRangePicker from "@wojtekmaj/react-daterange-picker";
+import React, { useState } from "react";
+import key from "weak-key";
 import styles from "./styles.module.scss";
 
-const title = 'Ты помог ответить на 20 вопросов',
-    notice = 'Уведомления',
-    noticeNumber = '5',
-    subtitle = 'История'
+const title = "Ты помог ответить на 20 вопросов",
+  notice = "Уведомления",
+  noticeNumber = "5",
+  subtitle = "История";
 
 const historyTitles = [
-    {
-        name: 'Дата',
-    },
-    {
-        name: 'Предмет',
-    },
-    {
-        name: 'Заданный вопрос',
-    },
-    {
-        name: 'Ответ',
-    },
-    {
-        name: 'Статус',
-    },
-]
+  {
+    name: "Дата",
+  },
+  {
+    name: "Предмет",
+  },
+  {
+    name: "Заданный вопрос",
+  },
+  {
+    name: "Ответ",
+  },
+  {
+    name: "Статус",
+  },
+];
 
 const tabel = [
-    {
-        link: '',
-        date: '20.05.2020',
-        dateTime: '14:47',
-        subject: 'Всеобщая история',
-        question: 'Как называлась программа М.С Горбачёва, проводима во внешней политике',
-        answer: 'Холодная война',
-        status: 'Пятёрочка',
-    },
-    {
-        link: '',
-        date: '20.05.2020',
-        dateTime: '14:47',
-        subject: 'Всеобщая история',
-        question: 'Как называлась программа М.С Горбачёва, проводима во внешней политике',
-        answer: 'Холодная война',
-        status: 'Пятёрочка',
-    },
-    {
-        link: '',
-        date: '20.05.2020',
-        dateTime: '14:47',
-        subject: 'Всеобщая история',
-        question: 'Как называлась программа М.С Горбачёва, проводима во внешней политике',
-        answer: 'Холодная война',
-        status: 'Пятёрочка',
-    },
-    {
-        link: '',
-        date: '20.05.2020',
-        dateTime: '14:47',
-        subject: 'Всеобщая история',
-        question: 'Как называлась программа М.С Горбачёва, проводима во внешней политике',
-        answer: 'Холодная война',
-        status: 'Пятёрочка',
-    },
-    {
-        link: '',
-        date: '20.05.2020',
-        dateTime: '14:47',
-        subject: 'Всеобщая история',
-        question: 'Как называлась программа М.С Горбачёва, проводима во внешней политике',
-        answer: 'Холодная война',
-        status: 'Пятёрочка',
-    },
-    {
-        link: '',
-        date: '20.05.2020',
-        dateTime: '14:47',
-        subject: 'Всеобщая история',
-        question: 'Как называлась программа М.С Горбачёва, проводима во внешней политике',
-        answer: 'Холодная война',
-        status: 'Пятёрочка',
-    },
-    {
-        link: '',
-        date: '20.05.2020',
-        dateTime: '14:47',
-        subject: 'Всеобщая история',
-        question: 'Как называлась программа М.С Горбачёва, проводима во внешней политике',
-        answer: 'Холодная война',
-        status: 'Пятёрочка',
-    },
-]
+  {
+    link: "",
+    date: "20.05.2020",
+    dateTime: "14:47",
+    subject: "Всеобщая история",
+    question: "Как называлась программа М.С Горбачёва, проводима во внешней политике",
+    answer: "Холодная война",
+    status: "Пятёрочка",
+  },
+  {
+    link: "",
+    date: "20.05.2020",
+    dateTime: "14:47",
+    subject: "Всеобщая история",
+    question: "Как называлась программа М.С Горбачёва, проводима во внешней политике",
+    answer: "Холодная война",
+    status: "Пятёрочка",
+  },
+  {
+    link: "",
+    date: "20.05.2020",
+    dateTime: "14:47",
+    subject: "Всеобщая история",
+    question: "Как называлась программа М.С Горбачёва, проводима во внешней политике",
+    answer: "Холодная война",
+    status: "Пятёрочка",
+  },
+  {
+    link: "",
+    date: "20.05.2020",
+    dateTime: "14:47",
+    subject: "Всеобщая история",
+    question: "Как называлась программа М.С Горбачёва, проводима во внешней политике",
+    answer: "Холодная война",
+    status: "Пятёрочка",
+  },
+  {
+    link: "",
+    date: "20.05.2020",
+    dateTime: "14:47",
+    subject: "Всеобщая история",
+    question: "Как называлась программа М.С Горбачёва, проводима во внешней политике",
+    answer: "Холодная война",
+    status: "Пятёрочка",
+  },
+  {
+    link: "",
+    date: "20.05.2020",
+    dateTime: "14:47",
+    subject: "Всеобщая история",
+    question: "Как называлась программа М.С Горбачёва, проводима во внешней политике",
+    answer: "Холодная война",
+    status: "Пятёрочка",
+  },
+  {
+    link: "",
+    date: "20.05.2020",
+    dateTime: "14:47",
+    subject: "Всеобщая история",
+    question: "Как называлась программа М.С Горбачёва, проводима во внешней политике",
+    answer: "Холодная война",
+    status: "Пятёрочка",
+  },
+];
 
 export const History = () => {
-    const [value, onChange] = useState([new Date(), new Date()]);
-    // Fri Oct 01 2021 00:00:00 GMT+0200 (Центральная Европа, летнее время)
-    // Fri Oct 22 2021 23:59:59 GMT+0200 (Центральная Европа, летнее время)
+  const [value, onChange] = useState([new Date(), new Date()]);
+  // Fri Oct 01 2021 00:00:00 GMT+0200 (Центральная Европа, летнее время)
+  // Fri Oct 22 2021 23:59:59 GMT+0200 (Центральная Европа, летнее время)
 
-    return (
-        <main className={styles.history}>
-            <div className={styles.container}>
-                <div className={styles.flex}>
-                    <h1>{title}</h1>
-                    <a href="#" className={styles.noticeLink}>{notice} <span>{noticeNumber}</span></a>
-                </div>
-                <div className={styles.flex}>
-                    <h2>{subtitle}</h2>
-                    <div className={styles.datepicker}>
-                        <DateRangePicker
-                            onChange={onChange}
-                            value={value} />
-                    </div>
-                </div>
-                <div className={styles.table}>
-                    <div className={styles.row}>
-                        {historyTitles.map((el) => (
-                            <div key={el}>
-                                {el.name}
-                            </div>
-                        ))}
-                    </div>
-                    {
-                        tabel.map((el) => (
-                            <a href={el.link} className={styles.row} key={el}>
-                                <div>
-                                    <b>{el.date}</b>{' в '}{el.dateTime}
-                                </div>
-                                <div>
-                                    {el.subject}
-                                </div>
-                                <div className={styles.question}>
-                                    <span>{el.question}</span>
-                                    <div className={styles.tooltip}>{el.question}</div>
-                                </div>
-                                <div>
-                                    {el.answer}
-                                </div>
-                                <div className={styles.status}>
-                                    {el.status}
-                                </div>
-                            </a>
-                        ))
-                    }
-                </div>
-            </div>
-        </main>
-    )
-}
+  return (
+    <main className={styles.history}>
+      <div className={styles.container}>
+        <div className={styles.flex}>
+          <h1>{title}</h1>
+          <a href="#" className={styles.noticeLink}>
+            {notice} <span>{noticeNumber}</span>
+          </a>
+        </div>
+        <div className={styles.flex}>
+          <h2>{subtitle}</h2>
+          <div className={styles.datepicker}>
+            <DateRangePicker onChange={onChange} value={value} />
+          </div>
+        </div>
+        <div className={styles.table}>
+          <div className={styles.row}>
+            {historyTitles.map((el) => (
+              <div key={key(el)}>{el.name}</div>
+            ))}
+          </div>
+          {tabel.map((el) => (
+            <a href={el.link} className={styles.row} key={key(el)}>
+              <div>
+                <b>{el.date}</b>
+                {" в "}
+                {el.dateTime}
+              </div>
+              <div>{el.subject}</div>
+              <div className={styles.question}>
+                <span>{el.question}</span>
+                <div className={styles.tooltip}>{el.question}</div>
+              </div>
+              <div>{el.answer}</div>
+              <div className={styles.status}>{el.status}</div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+};
