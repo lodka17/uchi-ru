85e102ca63525d95c94481de1c9d39701a457ce0
hstark <samebt15>
2021-10-02 14:52:53 +0300

    questions tabs component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import "./App.scss";
 import "./assets/fonts/fonts.css";
+import "antd/dist/antd.css";
 // import { RegistrationBL } from "./components-bl/RegistrationBL/RegistrationBL";
 // import { Registration } from "./components-bl/Registration/";
 // import { StartingPage } from "./components-bl/StartingPage/StartingPage";
 import { Header } from "./components/Header/Header";
 // import { History } from "./components/History/History";
-import { StartingForm } from "./components/StartingForm/StartingForm";
+// import { StartingForm } from "./components/StartingForm/StartingForm";
+import { Questions } from "./components/Questions/Questions";
 //   <History />
 const App = () => (
   <div className="App">
     <Header showMark={true} />
-    <StartingForm />
+    <Questions />
   </div>
 );
 
