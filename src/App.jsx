9eee66d5b263cc906b17e071795782104b513a89
lodka17 <lodka17@gmail.com>
2021-10-02 22:55:32 +0300

    feat(BLL): created store

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+/* eslint-disable */
+import { Button } from "antd";
+import { observer } from "mobx-react-lite";
+import React, {useEffect} from "react";
 import "./App.scss";
 import "antd/dist/antd.css";
 import "./assets/fonts/fonts.css";
@@ -9,15 +12,27 @@ import { BrowserRouter } from "react-router-dom";
 import { Header } from "./components/Header/Header";
 // import { History } from "./components/History/History";
 import { Routing } from "./components/Routing/Routing";
+import { userStore } from "./store/auth/AuthStore";
 
 //   <History />
-const App = () => (
-  <div className="App">
-    <BrowserRouter>
+const App = observer(() => {
+
+  useEffect(() => {
+      userStore.auth({
+          "username": "test88",
+          "password": "qsc_user@1"
+      })
+  }, [])
+
+  return (
+    <div className="App">
+      <BrowserRouter>
         <Header showMark={true} />
-        <Routing/>
-    </BrowserRouter>
-  </div>
-);
+        <Routing />
+      </BrowserRouter>
+    </div>
+  );
+
+});
 
 export default App;
