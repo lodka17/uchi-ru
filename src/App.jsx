c27eb2812ea29c132ad8822890f4cc3416033579
lodka17 <lodka17@gmail.com>
2021-10-02 09:45:41 +0300

    feature/content-fixed button

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./App.scss";
 import "./assets/fonts/fonts.css";
-import { RegistrationBL } from "./components-bl/RegistrationBL/RegistrationBL";
+// import { RegistrationBL } from "./components-bl/RegistrationBL/RegistrationBL";
 // import { Registration } from "./components-bl/Registration/";
 // import { StartingPage } from "./components-bl/StartingPage/StartingPage";
 import { Header } from "./components/Header/Header";
 // import { History } from "./components/History/History";
-// import { StartingForm } from "./components/StartingForm/StartingForm";
+import { StartingForm } from "./components/StartingForm/StartingForm";
 //   <History />
 const App = () => (
   <div className="App">
     <Header showMark={true} />
-    <RegistrationBL />
+    <StartingForm />
   </div>
 );
 
